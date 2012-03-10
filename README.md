e181be706c18699964e003df06364b9bc33f72e1
George Ornbo <george@shapeshed.com>
2012-03-10 12:06:08 +0000

    basic check for NaN on delay

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Traffic Jam
 
-[![Build Status](https://secure.travis-ci.org/shapeshed/trafficjam.png)](http://travis-ci.org/shapeshed/trafficjam)
-
-Traffic Jam is a simple web service for demonstrating slow networks where you want to actually make a network call rather than mock it in. 
+Traffic Jam is a simple web service for demonstrating slow networks. It can be used for demonstrating slow network traffic and explaining asynchrnous coding.
 
 Traffic Jam is hosted on Heroku. 
 
 # Usage
+
+Traffic Jam expects a query string of delay to indicate how long it should delay the response. This should be a number representing the milliseconds for the delay. Because Heroku throttles requests that take longer than 30 seconds the maximum delay is 28000.
+
+    curl -i "http://trafficjamapp.herokuapp.com/?delay=5000"
+
 
 
--- test/trafficjam.test.js
+++ test/trafficjam.test.js
@@ -27,7 +27,7 @@ describe('Making a get request', function(){
       })
     })
   })
-  describe('without a delay query string', function(){
+  describe('with a valid delay query string', function(){
     it('should get return a 200 response', function(done){
       http.get( { 
         path: '/?delay=1000',
@@ -48,6 +48,32 @@ describe('Making a get request', function(){
         });
         res.on('end', function () {
           assert.strictEqual(body, 'Response from Traffic Jam with 1000ms delay');
+          done();
+        });
+      })
+    })
+  })
+  describe('with a valid delay query string', function(){
+    it('should get return a 200 response', function(done){
+      http.get( { 
+        path: '/?delay=foobar',
+        port: '3000', 
+      }, function(res){
+        assert.strictEqual(res.statusCode, 422);
+        done();
+      })
+    })
+    it('should return the correct body content', function(done){
+      http.get( { 
+        path: '/?delay=foobar',
+        port: '3000', 
+      }, function(res){
+        var body = '';
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+        res.on('end', function () {
+          assert.strictEqual(body, 'delay must be a number');
           done();
         });
       })
