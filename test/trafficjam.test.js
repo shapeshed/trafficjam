7008f472b1455a805bd5bb82a3b3b850094130fb
George Ornbo <george@shapeshed.com>
2012-09-16 22:32:08 +0100

    favour classic JavaScript style

--- test/trafficjam.test.js
+++ test/trafficjam.test.js
@@ -1,6 +1,6 @@
-var http = require('http')
-, assert = require('assert')
-, tokenator = require('../lib/trafficjam');
+var http = require('http'),
+    assert = require('assert'),
+    tokenator = require('../lib/trafficjam');
 
 describe('Making a get request', function(){
   describe('without a query string', function(){
